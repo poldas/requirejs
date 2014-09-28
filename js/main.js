5caf0ac5f5ce7f6c50fb32bb7fd326dd4b7b1ff1
Daniel Polonis <poldas69@gmail.com>
2014-09-29 00:41:38 +0200

    markery: dodawanie, usuwanie, infowindow, eventy

--- js/main.js
+++ js/main.js
@@ -5,9 +5,45 @@ require.config({
     }
 });
 
-require(['jquery', 'modules/googlemaps'], function($, mapster) {
+require(['jquery', 'modules/Mapster'], function($, mapster) {
     $('h1').text('It\'s working!');
     var mapCanvas = $("#map-canvas").get(0);
     var mapa = mapster.create(mapCanvas);
+    mapa._on({
+		elem: mapa.gMap,
+		event: 'rightclick',
+		callback: function() {
+			alert('prawy klik');
+		}
+	});
+    var marker = mapa.addMarker({
+		lat: 37.791350,
+		lng: -122.435883,
+		draggable: true,
+		icon: "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-ff8a22/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/soccer.png",
+		id: 1,
+		content: "<div class='color'>Im open</div>",
+		event: {
+			name: 'click',
+			callback: function() {
+				alert('jestem klikniety');
+			}
+		}
+	});
+    var marker2 = mapa.addMarker({
+		lat: 37.771350,
+		lng: -122.565883,
+		draggable: true,
+		icon: "http://mapicons.nicolasmollet.com/wp-content/uploads/mapicons/shape-default/color-ff8a22/shapecolor-color/shadow-1/border-dark/symbolstyle-white/symbolshadowstyle-dark/gradient-no/soccer.png",
+		id: 1,
+		content: "<div class='color'>Im open</div>",
+		event: {
+			name: 'click',
+			callback: function() {
+				alert('jestem klikniety');
+			}
+		}
+	});
+    mapa._removeMarker(marker2);
 });
 
---
+++ js/modules/Mapster.js
@@ -0,0 +1,87 @@
+define(['async!http://maps.google.com/maps/api/js?sensor=false'], function() {
+	// moduł Mapster
+	var Mapster = (function() {
+		// domyślne opcje
+		var MAP_OPTIONS = {
+			disableDefaultUi: false,
+			scrollWheel: false,
+			draggable: true,
+			center: new google.maps.LatLng( 37.791350, -122.435883 ),
+			zoom: 10,
+			mapTypeId: google.maps.MapTypeId.HYBRID,
+			maxZoom: 15,
+			minZoom: 4
+		};
+		// konstruktor, ustawia mapę
+		function Mapster(elem, opts) {
+			opts = opts || MAP_OPTIONS;
+			this.gMap = new google.maps.Map(elem, opts);
+			this.markers = [];
+		};
+		
+		Mapster.prototype = {
+			// dodaje event
+			_on: function(opts) {
+				var self = this;
+				google.maps.event.addListener(opts.elem, opts.event, function(e) {
+					opts.callback.call(self, e);
+				});
+			},
+			// dodaje marker
+			addMarker: function(opts) {
+				var marker;
+				if(opts.lat && opts.lng) {
+					opts.position = {
+						lat: opts.lat,
+						lng: opts.lng,
+					}
+				}
+				marker = this._createMarker(opts);
+				this._addMarker(marker);
+				if (opts.event) {
+					this._on({
+						elem: marker,
+						event: opts.event.name,
+						callback: opts.event.callback
+					});
+				}
+				if (opts.content) {
+					this._on({
+						elem: marker,
+						event: 'click',
+						callback: function() {
+						    var infoWIn = new google.maps.InfoWindow({
+						    	content: opts.content
+						    });
+						    infoWIn.open(this.gMap, marker);
+						}
+					});
+				}
+				return marker;
+			},
+			
+			_addMarker: function(marker) {
+				this.markers.push(marker);
+			},
+			_removeMarker: function(marker) {
+				var indexOf = this.markers.indexOf(marker);
+				if(indexOf !== -1) {
+					this.markers.splice(indexOf, 1);
+					marker.setMap(null);
+				}
+			},
+			// tworzy obiekt markera
+			_createMarker: function(opts) {
+				opts.map = this.gMap;
+				return new google.maps.Marker(opts);
+			}
+		};
+		return Mapster;
+	}());
+	
+	// tworzy obiekt Mapstera
+	Mapster.create = function(elem, opts) {
+		return new Mapster(elem, opts);
+	}
+	return Mapster;
+});
